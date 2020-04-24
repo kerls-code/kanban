72ccc8868a7457be8d574782cdfe6fb7ff436149
Ker Lee Yap <kerlee.yap@uber.com>
2020-04-24 10:50:14 -0700

    Updates

---
+++ precache-manifest.295e9370cbbcf5ad1637d129cd5fb5a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa67810e23ebe33ebb54b256b921c436",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "dfe2b1ccffd018cf4b78",
+    "url": "/kanban/static/css/main.a82664ee.chunk.css"
+  },
+  {
+    "revision": "191f2c53af0f601ca38b",
+    "url": "/kanban/static/js/2.f609f546.chunk.js"
+  },
+  {
+    "revision": "dfe2b1ccffd018cf4b78",
+    "url": "/kanban/static/js/main.ca2b3437.chunk.js"
+  },
+  {
+    "revision": "4b0a68cbf5e664a91205",
+    "url": "/kanban/static/js/runtime-main.4737c27f.js"
+  }
+]);
