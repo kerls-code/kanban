3d021e4ee57925fa86679efb35b4b28f529fdf06
Ker Lee Yap <kerlee.yap@uber.com>
2020-04-24 10:41:44 -0700

    Pulling out components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-// import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import './App.css';
 
 // Copied from https://codesandbox.io/s/-w5szl?file=/src/index.js:0-4999
 
@@ -35,6 +35,10 @@ const move = (source, destination, droppableSource, droppableDestination) => {
 
   return result;
 };
+
+/**
+ * Styling
+ */
 const grid = 8;
 
 const getItemStyle = (isDragging, draggableStyle) => ({
@@ -44,19 +48,108 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   margin: `0 0 ${grid}px 0`,
 
   // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
+  background: isDragging ? "lightseagreen" : "darkseagreen",
 
   // styles we need to apply on draggables
   ...draggableStyle
 });
 const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
+  background: isDraggingOver ? "bisque" : "blanchedalmond",
   padding: grid,
   width: 250
 });
 
+/**
+ * Button to add new card
+ */
+ const AddNewCardButton = (props) => {
+  return <button
+    type="button"
+    onClick={() => props.onClick(props.columnIndex)}
+  >
+    Add new card
+  </button>
+ };
+
+ /**
+ * Adds a column/swimlane
+ */
+ const AddColumnButton = (props) => {
+  return <button
+        type="button"
+        onClick={() => props.onClick()}
+      >
+        Add column
+      </button>
+ };
+
+/**
+ * Card
+ */
+ const Card = (props) => {
+   const {item, index, ind, deleteCard} = props;
+ return <Draggable
+    key={item.id}
+    draggableId={item.id}
+    index={index}
+  >
+    {(provided, snapshot) => (
+      <div
+        className={'card'}
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        style={getItemStyle(
+          snapshot.isDragging,
+          provided.draggableProps.style
+        )}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around"
+          }}
+        >
+          <textarea defaultValue={item.content}></textarea>
+          <DeleteCardButton index={index} ind={ind} deleteCard={deleteCard}/>
+        </div>
+      </div>
+    )}
+  </Draggable>
+}
+
+/**
+ * Delete card button
+ */
+ const DeleteCardButton = (props) => {
+   const {index, ind, deleteCard} = props;
+   return <button
+      type="button"
+      onClick={() => deleteCard(ind, index)}
+    >
+      delete
+    </button>
+ }
+
+/**
+ * Main app
+ */
 function App() {
-  const [state, setState] = useState([getItems(10), getItems(5, 10)]);
+  const [state, setState] = useState([getItems(5), getItems(5, 10)]);
+  const addCardToColumn = columnIndex => {
+   const updatedState = [...state];
+   updatedState[columnIndex].push(getItems(1)[0]);
+   setState([...updatedState]);
+  };
+
+  const addColumn = () => setState([...state, []]);
+  const deleteCard = (ind, index) => {
+    const newState = [...state];
+    newState[ind].splice(index, 1);
+    setState(
+      newState.filter(group => group.length)
+    );
+  };
 
   function onDragEnd(result) {
     const { source, destination } = result;
@@ -85,22 +178,7 @@ function App() {
 
   return (
     <div>
-      <button
-        type="button"
-        onClick={() => {
-          setState([...state, []]);
-        }}
-      >
-        Add new group
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          setState([...state, getItems(1)]);
-        }}
-      >
-        Add new item
-      </button>
+      <AddColumnButton onClick={addColumn}/>
       <div style={{ display: "flex" }}>
         <DragDropContext onDragEnd={onDragEnd}>
           {state.map((el, ind) => (
@@ -111,45 +189,10 @@ function App() {
                   style={getListStyle(snapshot.isDraggingOver)}
                   {...provided.droppableProps}
                 >
+                  <AddNewCardButton onClick={addCardToColumn} columnIndex={ind}/>
+
                   {el.map((item, index) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.id}
-                      index={index}
-                    >
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={getItemStyle(
-                            snapshot.isDragging,
-                            provided.draggableProps.style
-                          )}
-                        >
-                          <div
-                            style={{
-                              display: "flex",
-                              justifyContent: "space-around"
-                            }}
-                          >
-                            {item.content}
-                            <button
-                              type="button"
-                              onClick={() => {
-                                const newState = [...state];
-                                newState[ind].splice(index, 1);
-                                setState(
-                                  newState.filter(group => group.length)
-                                );
-                              }}
-                            >
-                              delete
-                            </button>
-                          </div>
-                        </div>
-                      )}
-                    </Draggable>
+                    <Card item={item} index={index} ind={ind} deleteCard={deleteCard}/>
                   ))}
                   {provided.placeholder}
                 </div>
